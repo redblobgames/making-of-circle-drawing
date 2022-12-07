950e3c0ff0d3173bc48c66e42b92a02383b8b325
Amit Patel <redblobgames@gmail.com>
2022-12-07 11:30:50 -0800

    Feat: implemented (but haven't enabled) displaying diffs
    Problem: diff highlighting and syntax highlighting conflict for
    multi-line syntax. Not sure what to do about this yet.

--- making-of-circle-drawing.js
+++ making-of-circle-drawing.js
@@ -10,6 +10,48 @@ import Vue from './vue.v2.esm.browser.js';
 import {Prism, Diff} from './_build/lib.js';
 
 
+/** extract the <figure> section of the html, and de-indent */
+function extractFigure(html) {
+    let i = html.indexOf("<figure"),
+        j = html.indexOf("</figure");
+    let lines = html.slice(i, j).split('\n');
+    lines = lines.map(line => line.replace(/^    /, ""));
+    html = lines.join('\n') + "</figure>";
+    return html;
+}
+
+
+/** output diff lines with '+' or '-' or ' ' at the beginning */
+function calculateDiffs(oldText, newText) {
+    let oldLines = oldText.split('\n');
+    let newLines = newText.split('\n');
+    const isCommon = (aIndex, bIndex) => oldLines[aIndex] === newLines[bIndex];
+    let outputLines = [];
+    
+    let ai = 0, bi = 0
+    function record(type, lines) {
+        for (let line of lines) outputLines.push(`${type}${line}`);
+    }
+
+    function foundSubsequence(nCommon, aCommon, bCommon) {
+        // ai to aCommon are "deleted"
+        // bi to bCommon are "inserted"
+        // aCommon to aCommon+nCommon are "same"
+        // bCommon to bCommon+nCommon are "same"
+        record('-', oldLines.slice(ai, aCommon));
+        record('+', newLines.slice(bi, bCommon));
+        record(' ', oldLines.slice(aCommon, aCommon+nCommon));
+        ai = aCommon + nCommon;
+        bi = bCommon + nCommon;
+    }
+    
+    Diff(oldLines.length, newLines.length, isCommon, foundSubsequence);
+    record('-', oldLines.slice(ai));
+    record('+', newLines.slice(bi));
+
+    return outputLines.join('\n');
+}
+
 /* Highlight Vue v-bind and v-on */
 Prism.languages.markup.tag.addAttribute(
     /(?:v-|:|@)[-a-zA-Z]+/.source,
@@ -17,39 +59,59 @@ Prism.languages.markup.tag.addAttribute(
 );
 
 Vue.component('a-step', {
-    props: ['src', 'show-all'],
+    props: ['curr', 'prev', 'show-all'],
     data() {
         return {
-            tab: 'html',
-            html: "",
-            js: "",
+            tab: 'html', /* output, html, figure, js */
+            showDiff: false,
+            prevHtml: "",
+            prevJs: "",
+            currHtml: "",
+            currJs: "",
         };
     },
     template: `
     <div>
        <iframe v-if="tab === 'output'" :src="src"/>
-       <pre v-if="tab === 'html'"><code class="language-diff-html" v-html="syntaxHighlightedHtml"/></pre>
+       <pre v-else><code :class="languageClass" v-html="syntaxHighlightedText"/></pre>
     </div>
     `,
     computed: {
-        syntaxHighlightedHtml() {
-            let html = this.html;
-            if (!this.showAll) {
-                let i = this.html.indexOf("<figure"),
-                    j = this.html.indexOf("</figure");
-                let lines = this.html.slice(i, j).split('\n');
-                lines = lines.map(line => line.replace(/^    /, ""));
-                lines = lines.map(line => "+" + line); // HACK: for diff; need to actually implement diff
-                html = lines.join('\n') + "</figure>";
+        lang() {
+            return {html: "html", figure: "html", js: "javascript"}[this.tab];
+        },
+        languageClass() {
+            let diff = this.showDiff? "diff-" : "";
+            return `language-${diff}${this.lang}`;
+        },
+        prevFigure() { return extractFigure(this.prevHtml); },
+        currFigure() { return extractFigure(this.currHtml); },
+        syntaxHighlightedText() {
+            let [prev, curr] =
+                this.tab === 'html'? [this.prevHtml, this.currHtml]
+                : this.tab === 'figure'? [this.prevFigure, this.currFigure]
+                : this.tab === 'js'? [this.prevJs, this.currJs]
+                : ["??", "??"];
+            if (this.showDiff) {
+                return Prism.highlight(calculateDiffs(prev, curr),
+                                       Prism.languages.diff,
+                                       "diff-" + this.lang);
+            } else {
+                return Prism.highlight(curr,
+                                       Prism.languages[this.lang],
+                                       this.lang);
             }
-            return Prism.highlight(html, Prism.languages.diff, 'diff-html');
         },
     },
     async mounted() {
-        let loadHtml = fetch(this.src + "index.html");
-        let loadJs = fetch(this.src + "circle-drawing.js");
-        this.html = await (await loadHtml).text();
-        this.js = await (await loadJs).text();
+        let loadPrevHtml = fetch(this.prev + "index.html");
+        let loadPrevJs = fetch(this.prev + "circle-drawing.js");
+        let loadCurrHtml = fetch(this.curr + "index.html");
+        let loadCurrJs = fetch(this.curr + "circle-drawing.js");
+        this.prevHtml = await (await loadPrevHtml).text();
+        this.prevJs = await (await loadPrevJs).text();
+        this.currHtml = await (await loadCurrHtml).text();
+        this.currJs = await (await loadCurrJs).text();
     },
 });
 

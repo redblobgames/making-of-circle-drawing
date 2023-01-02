9980a380b67b5abe62ff93da0f834c0513ffcdf0
Amit Patel <redblobgames@gmail.com>
2023-01-02 15:06:39 -0800

    Feat: change coordinate system
    I moved the previous section with code in 18/ to 19/.

--- 19/circle-drawing.js
+++ 19/circle-drawing.js
@@ -118,11 +118,26 @@ for (let el of document.querySelectorAll("figure")) {
                     y: (this.measureLineLeft.y + this.measureLineRight.y) / 2
                 };
             },
+            bbox() {
+                return {
+                    left:   Math.ceil(this.center.q - this.radius) - 0.5,
+                    right:  Math.floor(this.center.q + this.radius) + 0.5,
+                    top:    Math.ceil(this.center.r - this.radius) - 0.5,
+                    bottom: Math.floor(this.center.r + this.radius) + 0.5,
+                };
+            },
         },
         methods: {
             distanceLabel(q, r) {
                 let distance = Math.hypot(q - this.center.q, r - this.center.r);
                 return distance.toFixed(distance >= 10? 0 : 1);
+            },
+            insideBounds(tile) {
+                // Note that bbox values have +/- 0.5 for the visuals
+                // but the tile values are integers so it should be ok
+                // for this computation too
+                return this.bbox.left <= tile.q && tile.q <= this.bbox.right
+                    && this.bbox.top <= tile.r && tile.r <= this.bbox.bottom;
             },
             // export these for use from the html template
             insideCircle,
